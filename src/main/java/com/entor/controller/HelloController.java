4350e6f2fbcb1d3d6aa50861a9f6561a88397386
Administrator <754040453@qq.com>
2020-11-30 16:31:06 +0800

    添加端口

--- src/main/java/com/entor/controller/HelloController.java
+++ src/main/java/com/entor/controller/HelloController.java
@@ -1,14 +1,22 @@
 package com.entor.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${server.port}")
+    private int port;
+
     @GetMapping("/hello")
     public String hello(){
         return "hello world";
     }
 
+    @GetMapping("/port")
+    public String port(){
+        return "端口号："+port;
+    }
 }
